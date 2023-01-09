316d676d21b1af60fde3d1d1f2623f206523f709
AddisonJPratt <pra21014@byui.edu>
2023-01-09 12:22:12 -0700

    uploading finished Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,31 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   // create random number generator
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(0, 100);
+
+        string input;
+        int guess;
+        //While statement that runs until you get the right number
+        do
+        {
+            Console.WriteLine("What is the magic number?");
+            input = Console.ReadLine();
+            guess = int.Parse(input);
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+        } while (guess != number);
+
     }
 }
