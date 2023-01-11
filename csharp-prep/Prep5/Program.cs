57b73c1a98efef8959a51c3f8c68c2a554465252
AddisonJPratt <pra21014@byui.edu>
2023-01-11 12:48:44 -0700

    Final updated prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int n = PromptUserNumber();
+        int squared = SquaredNumber(n);
+        Console.WriteLine($"{name}, the square of your number is {squared}");
+
+
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name:");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number:");
+        string num = Console.ReadLine();
+        int n = int.Parse(num);
+        return n;
+    }
+
+    static int SquaredNumber(int n)
+    {
+        int squared = n * n;
+        return squared;
     }
 }
