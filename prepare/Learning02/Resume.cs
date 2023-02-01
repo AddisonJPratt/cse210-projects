567c187542503b63b42787d457ea64cb3e5da798
AddisonJPratt <pra21014@byui.edu>
2023-02-01 04:19:39 -0700

    got the menu to work and figured out abstraction w/classes

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -12,8 +12,9 @@ public class Resume
         Console.WriteLine($"Name: {_name}");
         Console.WriteLine("Jobs: ");
 
-        foreach (Job job in _jobs)
+         foreach (Job job in _jobs)
         {
+            
             job.Display();
         }
 
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,8 +2,56 @@ using System;
 
 class Program
 {
+
+    public void Display()
+    {
+        Console.WriteLine("Please select one of the following choices");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+        Console.WriteLine("What would you like to do?");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Program main = new Program();
+        PromptGenerator p = new PromptGenerator();
+        p.RandomPrompt();
+        Console.WriteLine("Put in a journal entry question please");
+        string add = Console.ReadLine();
+        p.prompts.Add(add);
+        p.RandomPrompt();
+        main.Display();
+
+        int option = int.Parse(Console.ReadLine());
+        switch (option)
+        {
+            case 1:
+                Console.WriteLine("option 1");
+
+                break;
+            case 2:
+                Console.WriteLine("numba 2");
+
+                break;
+            case 3:
+                Console.WriteLine("numba 3");
+
+                break;
+            case 4:
+                Console.WriteLine("numba 4");
+
+                break;
+            case 5:
+                Console.WriteLine("numba 5");
+
+                break;
+            default:
+                Console.WriteLine("ok");
+
+                break;
+        }
+
     }
 }
