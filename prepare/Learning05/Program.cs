3afdad619150e6da112e5721c32a5891a79ab1e1
AddisonJPratt <pra21014@byui.edu>
2023-03-15 02:38:11 -0600

    finished Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> difShapes = new List<Shape>();
+
+        Square sq = new Square(12, "Green");
+        difShapes.Add(sq);
+
+        Rectangle rec = new Rectangle(6, 4, "Purple");
+        difShapes.Add(rec);
+
+        Circle circ = new Circle(6, "Orange");
+        difShapes.Add(circ);
+
+        foreach (Shape shap in difShapes)
+        {
+            Console.WriteLine(shap.GetColor());
+            Console.WriteLine(shap.GetArea());
+        }
+
     }
 }
